40b3c1b871830ed14f4ed7bdcb048aaf3b2d1d5c
aas-spec <aas.redhouse@gmail.com>
2021-02-01 16:18:15 +0300

    Optimized code

--- webik.go
+++ webik.go
@@ -3,7 +3,7 @@
 // - Handling HTTP GET "/" "" for index.html
 // - Processing GET for files with extensions: css, html, js
 //- Create reverse proxy for the backend
-// Written by Andrey Simanov 2020
+// Written by Andrey Simanov 2021
 
 package webik
 
@@ -28,40 +28,6 @@ type options struct {
 
 var opts options
 
-func index(w http.ResponseWriter, r *http.Request) {
-	pwd, _ := os.Getwd()
-	path := r.URL.Path
-	if opts.SourceApiRoute != "" && strings.Index(path, opts.SourceApiRoute) == 0 {
-		proxyHandler(w, r, opts.TargetApiRoute, opts.SourceApiRoute)
-		return
-	}
-start:
-	switch r.Method {
-	case "GET":
-		if path == "" || path == "/" {
-			path = "/index.html"
-		}
-		fileExt := filepath.Ext(path)
-		filePath := strings.Replace(path, "/", string(os.PathSeparator), -1)
-
-		if fileExt == "" {
-			path = "/index.html"
-			goto start
-		}
-		content, err := ioutil.ReadFile(pwd + string(os.PathSeparator) + opts.Path + string(os.PathSeparator) + filePath)
-		if err != nil {
-			http.NotFound(w, r)
-			return
-		} else {
-			contentType := mime.TypeByExtension(fileExt)
-			w.Header().Set("Content-Type", contentType)
-			_, err := w.Write(content)
-			if err != nil {
-				log.Printf("Warning: unable to write response: %v", err)
-			}
-		}
-	}
-}
 
 func ListenAndServe(Port string, SitePath string, TargetApiRoute string, SourceApiRoute string) {
 
@@ -83,13 +49,42 @@ func ListenAndServe(Port string, SitePath string, TargetApiRoute string, SourceA
 		TargetApiRoute: TargetApiRoute,
 	}
 
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", requestHandler)
 	log.Println("Webik Listen on port", opts.Port+"...")
 	err = http.ListenAndServe(opts.Port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	mainPath, _ := os.Getwd()
+	path := r.URL.Path
+	if opts.SourceApiRoute != "" && strings.Index(path, opts.SourceApiRoute) == 0 {
+		proxyHandler(w, r, opts.TargetApiRoute, opts.SourceApiRoute)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		fileExt := filepath.Ext(path)
+		if path == "" || path == "/" || fileExt == "" {
+			path = "/index.html"
+		}
+		filePath := strings.Replace(path, "/", string(os.PathSeparator), -1)
+		content, err := ioutil.ReadFile(mainPath + string(os.PathSeparator) + opts.Path + string(os.PathSeparator) + filePath)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		contentType := mime.TypeByExtension(fileExt)
+		w.Header().Set("Content-Type", contentType)
+		_, err = w.Write(content)
+		if err != nil {
+			log.Printf("Warning: unable to write response: %v", err)
+		}
+	}
+}
+
 
 func proxyHandler(res http.ResponseWriter, req *http.Request, TargetUrl string, SrcUrl string) {
 	targetUrl, _ := url.Parse(TargetUrl)
